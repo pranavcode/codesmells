41bf236854a7869e83d60057bde5ebd1fd70cf8c
Pranav Kulkarni <pranavcode@gmail.com>
2018-06-13 05:08:11 +0530

    Adding and fixing more data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
 var filters = [{
   "name": "Bloaters",
-  "description": "To be added."
+  "description":
+    "Bloaters are usually code which has grown to a considerable \
+    size that makes it difficult to work with and maintain in the \
+    long run. These could be methods as well as classes. These \
+    type of smells are not very evident right away. Instead, they \
+    start to grow over time, as the software evolves. Conscious \
+    effort is required by someone to make sure these type of smells \
+    are eradicated as an when possible and not ignored forever."
 }, {
   "name": "Tool Abusers",
-  "description": "To be added."
+  "description":
+    "Our primary programming paradigm in this context \
+    is object-oriented programming. These abusers are \
+    smells that are incorrect, inappropriate or sometimes incomplete \
+    applications of principles of object-orientated programming. \
+    Knowing things and unnecessarily applying them to places where \
+    they could be hazardous is not a sound way of approaching solutions."
 }, {
   "name": "Change Preventers",
-  "description": "To be added."
+  "description":
+    "Change preventers literally make code masters from making meaningful \
+    and required changes. These smells suggest that if you need to change \
+    something in one place of your code, you have to make significant \
+    changes in other areas too. As a result of which, the development \
+    cost goes up making it expensive and complicated."
 }, {
   "name": "Dispensables",
-  "description": "To be added."
+  "description":
+    "Dispensables are smells that represents things are not vital \
+    to the code. In other words, things that are pointless and unneeded. \
+    The code becomes cleaner, efficient and straightforward if \
+    these dispensable code pieces are removed. Which also enhances \
+    its readability."
 }, {
   "name": "Couplers",
-  "description": "To be added"
+  "description":
+    "Coupler smells represent the attraction behavior of two classes \
+    which could be called excessive. The way the code design helps in \
+    contributing to this excessive coupling between classes. Also, the \
+    smell could occur when disproportionate delegation happens from \
+    one class to another."
 }];
 
 var codesmells = [{
@@ -25,110 +53,147 @@ var codesmells = [{
     be unaware of the class that have a same functionality. Due to this \
     ignorance the programmer tends to create a new class or method that \
     exhibit equivalent functionality.",
-  "solutions": "If you can find the similarities between the two classes, you can \
+  "refactorings": "If you can find the similarities between the two classes, you can \
     often refactor the classes to make them share a common interface. Rename methods \
     to a common name in all the classes. Move them, add parameters and inject objects \
     to enable them to be moved to a common interface. You may move the methods to a \
     super-class and existing classes will become sub-classes. Its normal to come to \
-    a point where certain class seems to be redundant and easily removed.",
-  "refactorings": ""
+    a point where certain class seems to be redundant and easily removable.",
+  "references": "Move Method (Fowler, 142), Rename Method (Fowler, 273)"
 }, {
   "name": "Comments",
   "category": "Dispensables",
   "excerpt": "Methods or classes filled with explanatory comments.",
-  "description": "",
-  "reasons": "",
-  "solutions": "",
-  "refactorings": "",
+  "description": "Comments are created near the code to provide elaborate discussion about it. \
+    These are wisely positioned and decorated, but not necessarily required.",
+  "reasons": "These are written when the code master feels the need to explain \
+    the code as it is not very intuitive or straightforward. Such situations are \
+    not natural but human-made. Sometimes, hiding the holes in the fishy code \
+    that could be improved. Changing the structure of the code or better naming \
+    could make comments unnecessary.",
+  "refactorings": "If the comment talks about the section of code and explains it in \
+    further details, this section can be extracted as a method with a name that \
+    describes the code better. Expressions which needs comments to clarify further \
+    the operations happening within it could be extracted as separate variables with \
+    their subexpressions. Methods that carry comments with them are in need of a \
+    self-explanatory name, so please rename them. If some assumptions are required \
+    to allow code to word correct, check the assumptions directly instead of putting \
+    them as comments.",
+  "references": "Extract Method (Fowler, 110), Introduce Assertion (Fowler 267), \
+    Rename Method (Fowler, 273)",
 }, {
   "name": "Data Class",
   "category": "Dispensables",
-  "excerpt": "Class responsible only for holding fields and accessor methods without\
-    haveing additional functionality to operate on the data they own.",
-  "description": "",
-  "reasons": "",
-  "solutions": "",
+  "excerpt": "Class responsible only for holding fields and accessor methods without \
+    having additional functionality to operate on the data they own.",
+  "description": "These classes have data fields, and corresponding is getting and \
+    setting methods to read and modify that data. These methods are also called crude \
+    methods. These are merely used as containers of data required by other classes. \
+    These classes being dumb, are almost certainly being manipulated in far too much \
+    detail by others.",
+  "reasons": "These classes are the result of in-process extraction of objects as data \
+    objects but never get their operations. These classes are created by having public \
+    fields and accessing methods (getters and setters), not considering the real power \
+    of objects being into their behavior to operate and manage their data because they \
+    know it better.",
   "refactorings": "",
+  "references": "Move Method (Fowler, 142), Encapsulate Field (Fowler, 206), \
+    Encapsulate Collection (Fowler, 208)",
 }, {
   "name": "Data Clumps",
   "category": "Bloaters",
   "excerpt": "Pieces of information that flows together across methods or classes.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Class (Fowler, 149), Preserve Whole Object (Fowler, 288), \
+    Introduce Parameter Object (Fowler, 295)",
 }, {
   "name": "Divergent Change",
   "category": "Change Preventers",
-  "excerpt": "Situation that forces change in many unrelated class methods when changes are made to a class.",
+  "excerpt": "Situation that forces change in many unrelated class methods when \
+    changes are made to a class.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Class (Fowler, 149)",
 }, {
   "name": "Duplicate Code",
   "category": "Dispensables",
   "excerpt": "Almost identical pieces of code repeated in two or more places.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Method (Fowler, 110), Substitue Algorithm (Fowler, 139), \
+    Extract Class (Fowler, 149), Introduce Null Object (Fowler, 260), \
+    Pull Up Field (Fowler, 320), Pull Up Method (Fowler, 322), \
+    Form Template Method (Fowler, 345)",
 }, {
   "name": "Feature Envy",
   "category": "Couplers",
   "excerpt": "Classes that tends to work on data from other classes than their own.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references":
+    "Extract Method (Fowler, 110), Move Method (Fowler, 142), Move Field (Fowler, 146)",
 }, {
   "name": "Inappropriate Intimacy",
   "category": "Couplers",
   "excerpt": "Class uses private methods or data of another class.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references":
+    "Move Method (Fowler, 142), Move Field (Fowler, 146), \
+    Change Bidirectional Association to Unidirectional Association (Fowler, 200), \
+    Extract Class (Fowler, 149), Hide Delegate (Fowler, 157), \
+    Replace Inheritance with Delegation (Fowler, 352)",
 }, {
   "name": "Incomplete Library Client",
   "category": "Couplers",
   "excerpt": "Third-party library stops meeting project's upcoming needs.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Introduce Foreign Method (Fowler, 162), \
+    Introduce Local Extension (Fowler, 164)",
 }, {
   "name": "Large Class",
   "category": "Bloaters",
-  "excerpt": "Class have \"too\" many fields/methods/lines of code.",
+  "excerpt": "Class have 'too' many fields/methods/lines of code.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Class (Fowler, 149), Replace Data Value with Object (Fowler, 175), Extract Subclass (Fowler, 330), Extract Interface (Fowler, 341)",
 }, {
   "name": "Lazy Class",
   "category": "Dispensables",
   "excerpt": "Class that is maintained but does nothing.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Inline Class (Fowler, 154), Collapse Hierarchy (Fowler, 344)",
 }, {
   "name": "Long Method",
   "category": "Bloaters",
   "excerpt": "Method containing too many lines of code.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Method (Fowler, 110), Replace Temp with Query (Fowler, 120), \
+    Replace Method with Method Object (Fowler, 135), Decompose Conditional (Fowler, 238), \
+    Preserve Whole Object (Fowler, 288), Introduce Parameter Object (Fowler, 295)",
 }, {
   "name": "Long Parameter List",
   "category": "Bloaters",
   "excerpt": "Methods that take too many parameters as their arguments.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Preserve Whole Object (Fowler, 288), \
+    Replace Parameter with Method (Fowler, 292), \
+    Introduce Parameter Object (Fowler, 295)",
 }, {
   "name": "Message Chains",
   "category": "Couplers",
@@ -136,32 +201,38 @@ var codesmells = [{
     in between to accomplish specific thing.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Method (Fowler, 110), Move Method (Fowler, 142), \
+    Hide Delegate (Fowler, 157)",
 }, {
   "name": "Middle Man",
   "category": "Couplers",
   "excerpt": "Class that exits for a sole purpose of delegating work to other class.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Inline Method (Fowler, 117), Remove Middle Man (Fowler, 160), \
+    Replace Delegation with Inheritance (Fowler, 355)",
 }, {
   "name": "Parallel Inheritance Hierarchies",
   "category": "Change Preventers",
-  "excerpt": "Changes made to one set of hierarchical classes needs its replication into parallel classes.",
+  "excerpt": "Changes made to one set of hierarchical classes needs its \
+    replication into parallel classes.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Move Method (Fowler, 142), Move Field (Fowler, 146)",
 }, {
   "name": "Primitive Obsession",
   "category": "Bloaters",
   "excerpt": "Obsessive use of primitive types instead of small classes for simple tasks.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Class (Fowler, 149), Replace Data Value with Object (Fowler, 175), \
+    Replace Array With Object (Fowler, 186), Replace Type Code with Class (Fowler, 218), \
+    Replace Type Code with Subclasses (Fowler, 223), Replace Type Code with State/Strategy (Fowler, 227), \
+    Introduce Parameter Object (Fowler, 295)",
 }, {
   "name": "Refused Bequest",
   "category": "Tool Abusers",
@@ -169,41 +240,50 @@ var codesmells = [{
     classes, keeping unneeded methods unused or redefined to throw undefined exceptions.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Push Down Field (Fowler, 329), Push Down Method (Fowler, 322), \
+    Replace Inheritance with Delegation (Fowler, 352)",
 }, {
   "name": "Shotgun Surgery",
   "category": "Change Preventers",
-  "excerpt": "Adding a change in specific part of the code requires many small changes in many different classes.",
+  "excerpt": "Adding a change in specific part of the code requires many small \
+    changes in many different classes.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Move Method (Fowler, 142), Move Field (Fowler, 146), \
+    Inline Class (Fowler, 154)",
 }, {
   "name": "Speculative Generality",
   "category": "Dispensables",
-  "excerpt": "Code that is written in anticipation of future requirements and stays unused forever.",
+  "excerpt": "Code that is written in anticipation of future requirements and \
+    stays unused forever.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Inline Class (Fowler, 154), Rename Method (Fowler, 273), \
+    Remove Parameter (Fowler, 277), Collapse Hierarchy (Fowler, 344)",
 }, {
   "name": "Switch Statements",
   "category": "Tool Abusers",
   "excerpt": "Complex set of conditionals to perform next step.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Replace Type Code with Subclasses (Fowler, 223), \
+    Replace Type Code with State/Strategy (Fowler, 227), \
+    Replace Conditional with Polymorphism (Fowler, 255), \
+    Replace Parameter with Explicit Methods (Fowler, 285), \
+    Introduce Null Object (Fowler, 260)",
 }, {
   "name": "Temporary Field",
   "category": "Tool Abusers",
-  "excerpt": "Temporary properties that get their values under very specific circumstances,\
-    otherwise remains empty.",
+  "excerpt": "Temporary properties that get their values under very specific \
+    circumstances, otherwise remains empty.",
   "description": "",
   "reasons": "",
-  "solutions": "",
   "refactorings": "",
+  "references": "Extract Class (Fowler, 149), Introduce Null Object (Fowler, 260)",
 }]
 
 function countSmells() {
@@ -323,8 +403,8 @@ function generateCodeSmells() {
     node.find('.excerpt-text').text(codesmell['excerpt']);
     node.find('.description-text').text(codesmell['description']);
     node.find('.reasons-text').text(codesmell['reasons']);
-    node.find('.solutions-text').text(codesmell['solutions']);
     node.find('.refactorings-text').text(codesmell['refactorings']);
+    node.find('.references-text').text(codesmell['references']);
     $('.codesmells > .row').append(node);
   });
   reinstateBindings();
